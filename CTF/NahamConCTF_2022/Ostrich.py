95b0be3456a5b41ebdbba7c7c100e4e8ed9043a3
mohnad banat <75941340+mohnad-0b@users.noreply.github.com>
2022-05-01 07:09:28 +0300

    Create Ostrich.py

---
+++ CTF/NahamConCTF_2022/Ostrich.py
@@ -0,0 +1,24 @@
+import imageio
+from PIL import Image, GifImagePlugin
+from Crypto.Util.number import long_to_bytes as l2b, bytes_to_long as b2l
+import random
+from apng import APNG
+
+image_name = f'ostrich.jpg'
+image_open = Image.open(image_name)
+pixels = image_open.load()
+images = []
+pixels_orgin = image_open.load()
+
+for i in range(38):
+    image_name = f'./images/{i}.png'
+    image_open = Image.open(image_name)
+    pixels = image_open.load()
+    width, height = image_open.size
+    images = []
+    pixels = image_open.load()
+
+    for i in range(width):
+        for j in range(height):
+            if  pixels[i,j] != pixels_orgin[i,j]:
+                print(chr(b2l(l2b(pixels[i,j][0])+l2b(pixels[i,j][1]))//pixels_orgin[i,j][2]),end="")
