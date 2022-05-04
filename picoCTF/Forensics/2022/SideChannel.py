4fc5d6807675e972ca357d0d709eac3589679e85
mohnad banat <75941340+mohnad-0b@users.noreply.github.com>
2022-05-04 12:54:02 +0300

    Create SideChannel.py

---
+++ picoCTF/Forensics/2022/SideChannel.py
@@ -0,0 +1,22 @@
+import os
+from time import sleep , time
+import time
+tr = []
+P = ""
+password="aaaaaaaa"  
+for j in range(8) : 
+    start_time = time.time()
+    os.system(  "echo "+password+" | ./pin_checker")
+    bad_time =float(time.time() - start_time)
+    print("[+]"+"-"*100+"[+]")
+    for i in range(10) :
+        password = password[:j] + str(i) + password[j+1:] ; print("password is " + password)
+        start_time = time.time()
+        os.system("echo "+password+" | ./pin_checker")
+        run_time =float(time.time() - start_time)
+        tr.append(run_time)
+        sleep(0.5)
+    password = password[:j] + str(tr.index(max(tr))) + password[j+1:]
+    tr = [] 
+    print("-"*106)
+print(password)
