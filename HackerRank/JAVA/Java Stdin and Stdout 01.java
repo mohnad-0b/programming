0921f7d644fa75b91a01aca3d96483325a2e6262
mohnad banat <75941340+mohnad-0b@users.noreply.github.com>
2022-07-18 08:35:15 +0300

    Create  Java Stdin and Stdout 01.java

---
+++ HackerRank/JAVA/Java Stdin and Stdout 01.java
@@ -0,0 +1,15 @@
+//  Java Stdin and Stdout I
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int myInt0 = scanner.nextInt();
+        int myInt1 = scanner.nextInt();
+        int myInt2 = scanner.nextInt();
+        scanner.close();
+        System.out.println(myInt0);
+        System.out.println(myInt1);
+        System.out.println(myInt2);
+        return;
+    }
+}
