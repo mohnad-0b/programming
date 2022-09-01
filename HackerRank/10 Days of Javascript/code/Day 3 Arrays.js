0dc4aaca3ad83a97333b233b367c392cef6db979
mohnad banat <mohnadbanat00@gmail.com>
2022-09-01 16:57:05 +0300

    move codes to code folder

---
+++ HackerRank/10 Days of Javascript/code/Day 3 Arrays.js
@@ -0,0 +1,50 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+        
+    let max = Math.max(...nums)
+    while (true){
+        if (nums.indexOf(max) != -1){
+        nums.splice(nums.indexOf(max), 1)
+        }else {break}
+    }
+    let max_second = Math.max(...nums)
+    return max_second
+    
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
