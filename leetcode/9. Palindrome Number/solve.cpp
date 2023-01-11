1ba78320658f382afdc5aa1a431fc50bb20db744
mohnad banat <75941340+mohnad-0b@users.noreply.github.com>
2023-01-11 17:19:13 +0300

    Create solve.cpp

---
+++ leetcode/9. Palindrome Number/solve.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+      if (x < 0) {return false;}
+      int n = x,reversed_number=0,remainder;
+        while(n != 0) {
+          remainder = n % 10;
+          reversed_number = reversed_number * 10 + remainder;
+          n /= 10;
+  }
+
+  return (x==reversed_number);
+
+    }
+};
