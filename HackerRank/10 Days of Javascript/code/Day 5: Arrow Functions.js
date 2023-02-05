0a5042e46e8875a774893c059e16f16df82c1a8b
mohnad banat <75941340+mohnad-0b@users.noreply.github.com>
2023-02-05 03:34:29 +0300

    Create Day 5: Arrow Functions.js

---
+++ HackerRank/10 Days of Javascript/code/Day 5: Arrow Functions.js
@@ -0,0 +1,49 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    function sol(v){
+        if (v%2 == 0 ){
+            return v*2
+        } else{
+            return v*3
+        }
+    } 
+    
+    return nums.map(sol)
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
