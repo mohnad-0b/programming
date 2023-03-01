3b20eff97118f15bbfd88fbbb225fa0a71979445
mohnad banat <75941340+mohnad-0b@users.noreply.github.com>
2023-03-02 00:22:25 +0300

    Update Find_y.py

--- CTF/justctf2023/Crypto/MATH/Simple_Math/Find_y.py
+++ CTF/justctf2023/Crypto/MATH/Simple_Math/Find_y.py
@@ -12,3 +12,17 @@ if not isPrime(M):
     print(t[1])
 else :
     print(M/n)
+
+''' 
+How To find y in M = n%(y-1) ? 
+n = x*y .... (1) eq1
+M = n % (y-1)
+M = x*y % (y-1)
+M = [x%(y-1)*y%(y-1)]  ---> y%(y-1) = 1 
+M = x%(y-1)
+x = k(y-1) + M .... (2) eq2 # Note :  k = 1
+####### Solve equ ###########
+n = x*y
+n = y*[(y-1) + M]  from eq1 in eq2 
+y**2 + y*(M - 1 ) - n = 0 ; we have M and n :)
+'''
